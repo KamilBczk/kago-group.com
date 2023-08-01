1713b2395b9cc30757dc6c35f028d43dc16b723b
Kamil Biczyk <kamil.biczyk@wilink.be>
2023-08-02 00:02:24 +0200

    gtag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,19 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className="scroll-smooth">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-54X7PT7G');`,
-          }}
-        />
-      </Head>
       <body className={inter.className}>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54X7PT7G" height="0" width="0" style="display:none;visibility:hidden" />`,
-          }}
-        />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-54X7PT7G');
+      `}
+        </Script>
         <Header />
         {children}
         <Footer />
