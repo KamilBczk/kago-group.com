d078f09fa6e650411ffbf7325aa88c71c47fae43
Kamil Biczyk <kamil.biczyk@wilink.be>
2023-08-02 22:32:45 +0200

    buisness card integrations

---
+++ src/app/buisness-card/kamil-biczyk/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>Page Kamil peut être redirigée vers la page principale</div>;
+}
---
+++ src/app/buisness-card/mago-kitaev/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>Page Mago peut être redirigée vers la page principale</div>;
+}
