0276bc699d32d2567bc20344d1adc4024a6e0666
Kamil Biczyk <kamil.biczyk@wilink.be>
2023-09-14 22:58:24 +0200

    all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className="scroll-smooth">
+      <Head>
+        <script>
+          {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3655094,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+        </script>
+      </Head>
       <body className={inter.className}>
         {/* <Script id="google-tag-manager" strategy="afterInteractive">
           {`
