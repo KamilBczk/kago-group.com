e43730c9bfefe5ff1b4aae28f629b2e8eb021c12
Kamil Biczyk <65123540+KamilBczk@users.noreply.github.com>
2025-06-20 21:23:54 +0200

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,6 @@
 "use server";
-import { getAllFeaturedPostsForSitemap } from "../services/blog/index";
 
 export default async function sitemap() {
-  const posts = await getAllFeaturedPostsForSitemap();
   let sitemapResult = [
     {
       url: "https://kago-group.com/",
